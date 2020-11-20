d22ced307600d8f195ae562481a1eb36488b368f
Miguel Munoz <memg92@gmail.com>
2020-11-20 16:29:07 -0300

    need to fix dropdown from the profile button in css

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+
+const Navigation = ({ isLoaded }) => {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="profile-button-container">{isLoaded && sessionLinks}</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
