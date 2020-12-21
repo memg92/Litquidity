c47b79a1bbd8f30192d830a242a8980b3a960923
Miguel Munoz <memg92@gmail.com>
2020-12-20 22:18:58 -0300

    added trending stocks to the dashboard, which the user will be able to click and be redirected to stock details page

--- frontend/src/components/Dashboard/Stocks/TrendingStocks.js
+++ frontend/src/components/Dashboard/Stocks/TrendingStocks.js
@@ -1,33 +1,76 @@
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { fetch } from "../../../store/csrf";
+import "../News/News.css";
+import "./TrendingStocks.css";
 
-const TrendingStocks = () => {
-  const indexData = useSelector((state) => state.indices);
-  const spyNews = indexData.data.stockDataJSON.SPY.news;
+const TrendingStocks = (props) => {
+  const stockData = useSelector((state) => state.stocks);
+
+  const [searchInput, setSearchInput] = useState("");
+  const [stockLoaded, setStockLaoded] = useState(false);
+  const [stock, setStock] = useState({});
+  let trendData;
+  if (props.trend === "trending") {
+    trendData = stockData.data.mostActiveStocksJSON;
+  } else if (props.trend === "gainers") {
+    trendData = stockData.data.gainersStocksJSON;
+  } else if (props.trend === "losers") {
+    trendData = stockData.data.losersStocksJSON;
+  }
+
+  const handleClick = async (symbol) => {
+    setSearchInput(symbol);
+    // console.log(searchInput);
+    const res = await fetch("/api/stocks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ searchInput }),
+    });
+
+    if (res.ok) {
+      // const stock = res.data.stockDataJSON;
+      setStock(res.data.stockDataJSON);
+      // console.log("we hit this", stock);
+      setStockLaoded(true);
+    } else {
+      console.error("SYMBOL WAS NOT VALID");
+      throw res;
+    }
+  };
 
   return (
-    <div className="index-news-container">
-      <h2 className="index-news-title">Top Business News</h2>
-      {spyNews.map((article, idx) => {
-        return (
-          <div className={`article-${idx}`} key={article.url}>
-            <a href={article.url}>
-              <h3 className="index-news-headline">{article.headline}</h3>
-            </a>
-            <p className="index-news-source">{article.source}</p>
-            <p className="index-news-summary">
-              {article.summary.length > 150
-                ? article.summary.slice(0, 150).trim() + "... "
-                : article.summary + " "}
-              <a href={article.url}>Read More</a>
-            </p>
-            <img
-              className="index-news-image"
-              src={article.image}
-              alt={article.source}
-            />
-          </div>
-        );
-      })}
+    <div className="trending-stocks-container">
+      <h2 className="trending-stocks-title">
+        {`Top ${props.trend[0].toUpperCase() + props.trend.slice(1)}`}
+      </h2>
+      {console.log(trendData)}
+      <ul className="trending-stocks-list">
+        {trendData.map((stock) => {
+          return (
+            <li
+              className="stock-container"
+              onClick={(e) => handleClick(stock.symbol)}
+            >
+              <div className="stock-symbol">{stock.symbol}</div>
+              <div className="stock-price">{`$${stock.latestPrice}`}</div>
+              <div className="stock-change">
+                {`${parseFloat(stock.changePercent * 100).toFixed(2)}%`}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {stockLoaded && (
+        <Redirect
+          push
+          to={{
+            pathname: `/stocks/${stock.symbol}`,
+            state: { stock: stock },
+          }}
+        />
+      )}
     </div>
   );
 };
--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -6,6 +6,7 @@ import DOW from "./Indices/DOW";
 import NASDAQ from "./Indices/NASDAQ";
 import SP500 from "./Indices/SP500";
 import IndexNews from "./News/Indices";
+import TrendingStocks from "./Stocks/TrendingStocks";
 import "./Indices/Indices.css";
 import "./News/News.css";
 
@@ -23,10 +24,17 @@ const Dashboard = () => {
     <>
       {isLoaded && (
         <>
+          <h2 className="indices-message">Today's Markets</h2>
           <div className="indices-main-container">
             <SP500 />
             <NASDAQ />
             <DOW />
+          </div>
+          <div className="top-stocks-container">
+            <h2 className="top-stocks-message">Today's Top Stocks</h2>
+            <TrendingStocks trend="trending" />
+            <TrendingStocks trend="gainers" />
+            <TrendingStocks trend="losers" />
           </div>
           <IndexNews />
         </>
